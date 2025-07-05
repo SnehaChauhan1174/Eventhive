085659ed6a76a3ecf5f3100a3b462cf9b50c0ecb
sneha <snehachauhan5002@gmail.com>
2025-07-05 15:05:15 +0530

    Added owner authorization mw and place validate event and valid review

--- middleware.js
+++ middleware.js
@@ -1,3 +1,8 @@
+const Events=require('./models/eventListing');
+const {eventSchema,reviewSchema}=require('./schema.js');
+const ExpressError=require('./utils/ExpressError');
+
+
 module.exports.isLoggedIn=(req,res,next)=>{
     if(!req.isAuthenticated()){
         req.session.redirectUrl=req.originalUrl;
@@ -12,4 +17,34 @@ module.exports.saveRedirectUrl=(req,res,next)=>{
     }
     next();
     
+}
+
+module.exports.isOwner=async(res,req,next)=>{
+    let {id}=req.params;
+    let event = await Events.findById(id);
+    if(!event.organizer.equals(res.locals.currUser._id)){
+        req.flash("error","You are not an organizer of this event!");
+        return res.redirect(`/events/${id}`);
+    }
+    next();
+}
+
+module.exports.vaildateEvent=(req,res,next)=>{
+    let {error}=eventSchema.validate(req.body);
+    if(error){
+        let errMsg=error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
+}
+
+module.exports.vaildateReview=(req,res,next)=>{
+    let {error}=reviewSchema.validate(req.body);
+    if(error){
+        let errMsg=error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
 }
